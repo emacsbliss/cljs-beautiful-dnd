708e1516f25e060a2178b716a503a5e860bf6f36
emacsbliss <emacsbliss@gmail.com>
2020-11-19 08:23:35 -0500

    style task during dragging

--- src/cljs/dragdrop/task.cljs
+++ src/cljs/dragdrop/task.cljs
@@ -5,12 +5,12 @@
    [stylefy.core :refer [use-style]])
 )
 
-(def container-style
+(defn container-style [state]
   {:margin-bottom "8px"
    :border "1px solid lightgrey"
    :border-radius "2px"
    :padding "8px"
-   :background-color "white"
+   :background-color (if (:is-dragging state) "lightgreen" "white")
 })
 
 (defn- container [props & child]
@@ -20,8 +20,13 @@
   ;; we also need the :style from props along with the other
   ;; style properties we have defined, otherwise drag won't work
   (let [style-props (:style props)
-        merged-style (merge style-props container-style)
-        other-props (dissoc props :style)]
+        merged-style (merge style-props (container-style props))
+        other-props (-> props
+                        (dissoc :style)
+                        ;; NOTE: without removing this key, will get warning:
+                        ;; Warning: React does not recognize the `isDragging` prop on a DOM element.
+                        (dissoc :is-dragging)
+                        )]
   [:div (use-style merged-style other-props)
     child])
 )
@@ -32,16 +37,17 @@
     :index index}
 
     ;; NOTE: this has to be a function otherwise will get error
-    (fn [provided]
+    (fn [provided snapshot]
        (let [inner-ref (.-innerRef provided)
              draggable-props (-> (.-draggableProps provided)
                                 (js->clj :keywordize-keys true))
              drag-hanlder-props (-> (.-dragHandleProps provided)
                                 (js->clj :keywordize-keys true))
-             ]
+             is-dragging (.-isDragging snapshot)]
 
        (ra/as-element
-          [container (-> {:ref inner-ref}
+        [container (-> {:ref inner-ref
+                        :is-dragging is-dragging}
                            (merge draggable-props)
                            (merge drag-hanlder-props))
         (:content tsk)])
